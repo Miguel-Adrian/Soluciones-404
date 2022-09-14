bf007a9d3fb9b829cde75d0f2c9c4ba8a5bafc99
Miguel Adrián Núñez <miguel.adrian.nunez@gmail.com>
2022-09-13 21:11:40 -0500

    chore | Se añade barra de navegación a página de contacto.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-function suma (a,b){
-    return a+b;
-}
+let btnValidar = document.getElementById("btnValidar");
+let campoEmail = document.getElementById("validationServer031");
+
+
+btnValidar.addEventListener("click", function(e){
+    e.preventDefault();
+    
+
+    if ((campoEmail.value.length !== 0)&& (campoEmail == (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/))){
+        console.log(campoEmail.value.length);
+        campoEmail.classList.remove("is-invalid");
+        campoEmail.classList.add("is-valid");
+       } else {
+        campoEmail.classList.remove("is-valid");
+        campoEmail.classList.add("is-invalid");
+       }
+})
+
+function sendEmail(){
+    Email.send({
+     Host : "smtp.gmail.com",
+     Username : document.getElementById(validationServer01).value,
+     Password : "password",
+     To : 'qpets.officialstore@gmail.com',
+     From : document.getElementById(validationServer031).value,
+     Subject : "This is the subject",
+     Body : "And this is the body",
+ }).then(
+   message => alert(message)
+ );
+ }
+
