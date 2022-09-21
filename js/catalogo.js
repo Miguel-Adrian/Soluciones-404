d785d1bbef5a74aa18e43822b23ba412469c5109
Miguel Adrián Núñez <miguel.adrian.nunez@gmail.com>
2022-09-21 13:58:02 -0500

    Se añaden, al archivo 'catalogo.js', los objetos correspondientes a las categorías 'toys' y 'accesories' que, asu vez, están englobadas en la supercategoría 'dog'. Se añaden los elementos correspondientes a las imágenes de dichos objetos al directorio 'src/'.

---
+++ js/catalogo.js
@@ -0,0 +1,47 @@
+
+//1. Pelota de caucho
+addItem({'id':1,'name':'Pelota K-Nina',
+    'img':'.//src/images/catalogo/DogTretBallPink.png',
+    'description':'Pelota de caucho rosa, ¡puede almacenar premios de comida en su interior, ya que cuenta con un compartimento! Útil para ejercitar a tu mascota y enseñarle uno que otro truco de una sola vez.',
+    'price': 123.50,'pet':'dog','category':'toys'});
+
+//2. Dona de tela con silbato
+addItem({'id':2,'name':'Dona chillona',
+    'img':'../src/images/catalogo/DogSqueakyDonut.png',
+    'description':'El juguete emite un chillido cuando es presionada, algo que le encantará a tu perro, sea cachorro o adulto.',
+    'price':199.00,'pet':'dog','category':'toys'})
+//3. Hueso de caucho
+addItem({'id':3,'name':'Huesito Fiu-Fiu',
+'img':'../src/images/catalogo/DogRubberBone.png',
+'description':'Hueso de caucho de uso rudo. Ideal para canes adultos. Este juguete puede ser útil para evitar que tu perro muerda objetos que no debería morder. Antiestrés.',
+'price':120.00,'pet':'dog','category':'toys'})
+
+//4. Juguete de fibra
+addItem({'id':4,'name':'Juguete de fibra',
+'img':'../src/images/catalogo/DogRopeToy.png',
+'description':'Juguete de fibra 100% natural. Ideal para canes adultos. Dale a tu amigo horas de diversión y relajamiento con este juguete.',
+'price':99.00,'pet':'dog','category':'toys'})
+
+//5. Kit para masticar
+addItem({'id':5,'name':'Kit de sandalias y zapatos',
+'img':'../src/images/catalogo/DogKitSandalsSneakersChewies.png',
+'description':'Pensado principalmente para cachorros, haz que éste pase horas de diversión masticando su juguete y no tu calzado diario. Precaución: exceso de dulzura al ver a tu amiguito pasándola bien.',
+'price':216.00,'pet':'dog','category':'toys'})
+
+//21. Camita para perro
+addItem({'id':21,'name':'Camita P-rruna',
+'img':'../src/images/catalogo/DogBed1.png',
+'description':'¿Un día agotador? Dale el descanso que se merece a tu peludo con esta preciosa cama en forma de sandalia. Hecha con materiales 100% naturales, le dará a tu amig@ un descanso reparador.',
+'price':1314.00,'pet':'dog','category':'toys'})
+
+//22. Casita para perro una planta
+addItem({'id':22,'name':'K-sita K9',
+'img':'../src/images/catalogo/DogHouse1.png',
+'description':'Pensado principalmente para cachorros, haz que éste pase horas de diversión masticando su juguete y no tu calzado diario. Precaución: exceso de dulzura al ver a tu amiguito pasándola bien.',
+'price':5163,'pet':'dog','category':'accesory'})
+
+//23. Casita para perro 2 plantas
+addItem({'id':23,'name':'K-sita K9 Suite de 2 plantas',
+'img':'../src/images/catalogo/DogBed2.png',
+'description':'¿Acaso hay más un un miembro peludo en la familia? ¡No hay problema! Esta hermosa suite para dos amiguit@s está pensada exclusivamente para ahorrar espacio.',
+'price':10011.00,'pet':'dog','category':'accesory'})
