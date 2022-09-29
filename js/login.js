362e477bbff5c2b71aeab774e3b8a63ff57c7bcd
Maurine Garcia <2153066152@cua.uam.mx>
2022-09-28 21:03:19 -0500

    Chore | se agregó la funcionalidad de localStorage al formulario de login

--- js/login.js
+++ js/login.js
@@ -1,50 +1,40 @@
-const container = document.querySelector(".container"),
-      pwShowHide = document.querySelectorAll(".showHidePw"),
-      pwFields = document.querySelectorAll(".password"),
-      signUp = document.querySelector(".signup-link"),
-      login = document.querySelector(".login-link");
+// inicializa el contador para usuarios 
+id =1;
+let usuarios = [];
 
-    //   js code to show/hide password and change icon
-    pwShowHide.forEach(eyeIcon =>{
-        eyeIcon.addEventListener("click", ()=>{
-            pwFields.forEach(pwField =>{
-                if(pwField.type ==="password"){
-                    pwField.type = "text";
+//Arreglo para cada usuario 
 
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye-slash", "uil-eye");
-                    })
-                }else{
-                    pwField.type = "password";
+class User {
+    constructor (nombre, apellido, correo, numero, contrasenia1, contrasenia2){
+       this.nombre = nombre;
+       this. apellido = apellido;
+       this.correo = correo;
+       this.numero = numero;
+       this.contrasenia1 = contrasenia1;
+       this.contrasenia2 = contrasenia2;
 
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye", "uil-eye-slash");
-                    })
-                }
-            }) 
-        })
-    })
-
-    // js code to appear signup and login form
-    signUp.addEventListener("click", ( )=>{
-        container.classList.add("active");
-    });
-    login.addEventListener("click", ( )=>{
-        container.classList.remove("active");
-    });
-//validacion del campo email
-function isEmailValid(email) {
-    let regex =
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return !!regex.exec(email);
+    }
 }
 
-if (isEmailValid(inputdireccion.value)) {
-    cont++;
+//Evento submit para el boton 
+document.getElementById("btnRegistrar").addEventListener("click", function(e) {
+    e.preventDefault();
+   
+   let newUserName = document.getElementById("newUserName");
+   let newUserLastName = document.getElementById("newUserLastName");
+   let newUserEmail = document.getElementById("newUserEmail");
+   let newUserNumber = document.getElementById("newUserNumber");
+   let newUserPass1 = document.getElementById("newUserPass1");
+   let newUserPass2 = document.getElementById("newUserPass2"); 
+   
 
-    inputdireccion.classList.remove("is-invalid");
-    inputdireccion.classList.add("is-valid");
-} else {
-    inputdireccion.classList.remove("is-valid");
-    inputdireccion.classList.add("is-invalid");
-}
+    const key = "info";
+    let cont = 0;
+
+    //localStorage 
+    usuarios.push(new User (newUserName.value, newUserLastName.value,newUserEmail.value,
+        newUserNumber.value, newUserPass1.value, newUserPass2.value));
+    id++;
+    localStorage.setItem(key, JSON.stringify(usuarios))
+
+});
